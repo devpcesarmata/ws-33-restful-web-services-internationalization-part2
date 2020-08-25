a2370d4b2fefa67ffd927221da871213622d560c
César <dev.pcesarmata@gmail.com>
2020-08-25 16:25:31 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws33restfulwebservicesinternationalizationpart2/Ws33RestfulWebServicesInternationalizationPart2Application.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws33restfulwebservicesinternationalizationpart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws33RestfulWebServicesInternationalizationPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws33RestfulWebServicesInternationalizationPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws33restfulwebservicesinternationalizationpart2/Ws33RestfulWebServicesInternationalizationPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws33restfulwebservicesinternationalizationpart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws33RestfulWebServicesInternationalizationPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
